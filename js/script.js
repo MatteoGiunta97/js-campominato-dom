fa106e4607274e7432c4fd6413940a8c76d9472a
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-18 17:15:05 +0100

    feat: add starting comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+/*
+Consegna:
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: 
+le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, 
+perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri 
+generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. 
+Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle 
+altre celle.
+
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero 
+massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non 
+sono bombe).
+
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte 
+che l’utente ha cliccato su una cella che non era una bomba.
+*/
+
+
 
 const mainGrid = document.querySelector('#ms-grid');
 const playBtn = document.querySelector('#ms-play-btn');
@@ -49,9 +70,13 @@ function createNewGame() {
 // cellsPerRow -> numero intero che rappresenta quanti quadrati ci saranno per row
 // return: un elemento del DOM che rappresenta il singolo square creato
 function generateSquare(number, cellsPerRow) {
+
     const newSquare = document.createElement('div');
+
     newSquare.classList.add('ms-square')
+
     newSquare.innerHTML = number;
+
     newSquare.style.width = `calc(100% / ${cellsPerRow})`;
     newSquare.style.height = `calc(100% / ${cellsPerRow})`;
 
