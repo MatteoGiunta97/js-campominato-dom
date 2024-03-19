b8a57815756541c3c2c1d946fba54af37f22ae8a
MatteoGiunta97 <matteogiunta@outlook.it>
2024-03-19 10:49:36 +0100

    feat: add points count and game over

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ che l’utente ha cliccato su una cella che non era una bomba.
 
 const mainGrid = document.querySelector('#ms-grid');
 const playBtn = document.querySelector('#ms-play-btn');
+const message = document.querySelector('#ms-user-message')
 playBtn.addEventListener('click', createNewGame);
 
 // Funzione che gestisce il gioco
@@ -29,6 +30,7 @@ function createNewGame() {
     
     // Svuoto la griglia quando viene generata una nuova partita
     mainGrid.innerHTML = '';
+    message.innerHTML = '';
 
     // Quando l'utente clicca play leggo il valore della select
     const level = document.querySelector('#ms-level').value;
@@ -53,59 +55,75 @@ function createNewGame() {
     // di square/difficoltà
     let bombsArray = [];
     while (bombsArray.length < 16) {
-        
         let bombNumber = numRandom(1,numberOfSquares);
-    
         // Inserisco i numeri digitati nell'array se non sono già presenti in esso
-    
         if (bombsArray.includes(bombNumber) === false) {
-    
             bombsArray.push(bombNumber);
         }
-    
     }
+    console.log(bombsArray)
+
+    // Creo la variabile points per tenere conto del punteggio
+    // la variabile maxPoints per il numero massimo di punti 
+    // e la variabile gameOver per il reset
+    let points = 0;
+    const maxPoints = numberOfSquares - 16;
+    let gameOver = false;
 
     // Per numberOfSquares volte voglio creare uno square
     for (let i = 1; i <= numberOfSquares; i++) {
         const thisNumber = i;
 
         const square = generateSquare(thisNumber, numberOfCellsPerRow);
+
+        // Gestisco il click sulla cella
         square.addEventListener('click', function() {
-            // Aggiungo la classe clicked e clickedBomb, la cella si colora in bianco quando  
-            // non è bomba e in rosso se lo è
-            if (bombsArray.includes(thisNumber)) {
-
-                this.classList.add('ms-clickedBomb');
-            } else {
-
-                this.classList.add('ms-clicked');
-            }
-            
+            if (!gameOver) {
+                // Aggiungo la classe clicked e clickedBomb, la cella si colora in bianco quando  
+                // non è bomba e in rosso se lo è
+                if (bombsArray.includes(thisNumber)) {
+                    this.classList.add('ms-clickedBomb');
+                    // Il gioco termina e comunico il risultato
+                    message.innerHTML = `Hai perso, il tuo punteggio è ${points}`;
+                    gameOver = true;
+                } else {
+                    this.classList.add('ms-clicked');
+                    // Incremento points
+                    points++;
+                    //  Quando raggiungo maxpoints il gioco termina e comunico il risultato
+                    if (points === maxPoints) {
+                        message.innerHTML = `Hai vinto, il tuo punteggio è ${points}`;
+                        gameOver = true;
+                    }      
+                }  
+            } 
         });
         mainGrid.append(square);
     }
 }
 
-// Genera un elemento del DOM che rappresenta uno square
+
+
+
+// FUNZIONI:
+
+// La funzione genera un elemento del DOM che rappresenta uno square
 // number -> il numero intero della cella
 // cellsPerRow -> numero intero che rappresenta quanti quadrati ci saranno per row
 // return: un elemento del DOM che rappresenta il singolo square creato
 function generateSquare(number, cellsPerRow) {
 
     const newSquare = document.createElement('div');
-
     newSquare.classList.add('ms-square')
-
     newSquare.innerHTML = number;
-
     newSquare.style.width = `calc(100% / ${cellsPerRow})`;
     newSquare.style.height = `calc(100% / ${cellsPerRow})`;
-
     return newSquare;
 }
 
 
-// La funzione genera un numero intero random tra due parametri: min e max
+// La funzione genera un numero intero random tra due parametri: min e max(incluso)
 function numRandom(min, max) {
+
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
